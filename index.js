3e4c7b407f3c36814ff840677659bca354d7f8e6
Kylie Clark <kyliedarlene@icloud.com>
2024-01-03 11:25:22 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code here...
+
+const menuItems = document.getElementById('menu-items');
+const dishImage = document.getElementById('dish-image');
+const dishName = document.getElementById('dish-name');
+const dishDescription = document.getElementById('dish-description');
+const dishPrice = document.getElementById('dish-price');
+
+fetch("http://localhost:3000/menu")
+    .then((response) => response.json())
+    .then((menu) => {
+        menu.forEach((item) => {
+            // challenge 1
+            const newMenuItem = document.createElement('span');
+            newMenuItem.innerText = item.name;
+            menuItems.append(newMenuItem);
+
+            // challenge 2
+            displayMenuItem(menu[0]);
+
+            // challenge 3
+            newMenuItem.addEventListener('click', () => {
+                displayMenuItem(item);
+            })
+        })
+    })
+
+function displayMenuItem(source) {
+    dishImage.src = source.image;
+    dishName.innerText = source.name;
+    dishDescription.innerText = source.description;
+    dishPrice.innerText = source.price;
+}
+
+// challenge 4
+
+const form = document.getElementById('cart-form');
+const cartNumDisplay = document.getElementById('number-in-cart');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const cartNumAsNum = Number(cartNumDisplay.innerText);
+    const addToCartNum = Number(event.target.number.value);
+    cartNumDisplay.innerText = cartNumAsNum + addToCartNum;
+})
